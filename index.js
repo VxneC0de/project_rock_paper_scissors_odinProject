79dabf383559c04bebcef716780a6d2370d2fb8f
VxneC0de <vanerubio2020@gmail.com>
2024-06-03 16:49:00 -0400

    feat: add new code for index.js

--- index.js
+++ index.js
@@ -1,3 +1,36 @@
-function getComputerChoice(){
-    
+function getComputerChoice() {
+  const options = ["rock", "paper", "scissors"];
+  const randomIndex = Math.floor(Math.random() * options.length);
+  return options[randomIndex];
 }
+
+const computerChoice = getComputerChoice();
+console.log(computerChoice);
+
+function getHumanChoice(){
+
+  let humanChoice = prompt("Choose rock, paper, scissors").toLocaleLowerCase();
+
+  while(humanChoice !== "rock" || humanChoice !== "paper" || humanChoice !== "scissors" || humanChoice === null){
+    alert("Please enter a valid option")
+    getComputerChoice();
+  }
+
+}
+
+getHumanChoice();
+
+
+
+
+/*if(humanChoice === null){
+    return null; // Si el usuario hace clic en cancelar en el cuadro de diálogo, detenemos la función.
+  }
+
+
+  while(humanChoice !== "rock" || humanChoice !== "paper" || humanChoice !== "scissors"){
+    
+  }
+  */
+
+
